b1260df6e4e739027f42b33f7ca27f63f881742e
M <m@Ms-MacBook-Pro.local>
2020-02-15 12:30:31 -0700

    Adds BrowserRouter and paths for the logged in users

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import Header from '../Header/Header'
+import {Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <main className="App">
-      <Header/>
+
+    <Route
+         exact
+         path="/"
+         render={() => {
+           return (
+            <>
+             <Header/>
+            </>
+         )
+         }}
+       />
+
+    <Route
+        exact
+        path="/find"
+        render={() => {
+          return (
+            <>
+              <Header/>
+            </>
+          )
+          }}
+        />
+
+     <Route
+          exact
+          path="/account"
+          render={() => {
+            return (
+              <>
+                <Header/>
+              </>
+            )
+            }}
+          />
+          
+      <Route
+          exact
+          path="/schedule"
+          render={() => {
+            return (
+                <>
+                 <Header/>
+                </>
+             )
+             }}
+             />
+
     </main>
+
+
   );
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from '../Imgs/logo.svg'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
     <nav className="header">
       <div className="nav-left">
-        <img className="logo" alt='logo' src={logo}/>
-        <span className="logo-text">NOTARY NOW</span>
+        <Link to="/"> <img className="logo" alt='logo' src={logo}/></Link>
+        <Link to="/"><span className="logo-text">NOTARY NOW</span></Link>
       </div>
 
       <div className="nav-right">
-        <button className="nav-button">FIND</button>
-        <button className="nav-button">INBOX</button>
-        <button className="nav-button">SCHEDULE</button>
-        <button className="nav-button">ACCOUNT</button>
+        <Link to="/find">  <button className="nav-button">FIND</button> </Link>
+        <Link to="/schedule">  <button className="nav-button">SCHEDULE</button> </Link>
+        <Link to="/account">  <button className="nav-button">ACCOUNT</button> </Link>
       </div>
     </nav>
   );
