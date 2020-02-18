965e2ee29e89b958932a38e066ddc39d2801e4c1
M <m@Ms-MacBook-Pro.local>
2020-02-18 12:20:08 -0700

    Test Travis 5

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Header from '../Header/Header';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1).toBe(1);
+});
