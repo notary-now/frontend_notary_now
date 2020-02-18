58d9808217a70b3134f21ce5d81fbd161e4fecf2
M <m@Ms-MacBook-Pro.local>
2020-02-18 14:19:37 -0700

    Add styling for profile page

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -1,28 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import cory from '../Imgs/cory.jpeg'
+import logo from '../Imgs/logo.svg'
 
 
 function Account() {
   let [account, setAccount] = useState({})
 
-  useEffect(() => {setAccount({
-  img: 'string',
-  firstName: 'string',
-  lastName:'string',
-  bio:'string',
-  location: '80444',
-  startCommission:'1/2/12',
-  endCommission: '1/1/2222',
-  verfied: false
-})},[])
+  useEffect(() => {
+
+  fetch("https://backend-notary-now.herokuapp.com/api/v1/user/1/")
+    .then(response => response.json())
+    .then(result => setAccount(result))
+    .catch(error => console.log('error', error));
+    }
+  ,[])
 
   return (
     <section className="account">
-      <span>{account.firstName} {account.lastName}</span>
-      <img className="account-img" src={cory} alt='notary'/>
-      <span>{account.bio}</span>
-      <span>{account.location}</span>
-      <span>{account.startCommission} - {account.endCommission}</span>
+      <span className='notary-name'>{account.first_name} {account.last_name}</span>
+      <span><img className="account-img" src={account.profile_photo} alt='notary'/><img className="verified" alt='verified' src={logo}/></span>
+      <span className="bio">Bio: I have been notarizing since a young age, I am currently based in Highlands Ranch. I am willing to travel as far as the DTC to meet to notarize. I prefer to work from 5:30-7:30 M - F.</span>
+      <span className="bio">Location: 80224</span>
+      <span className="bio">Commission Dates: 1/2/2020 - 1/2/2222</span>
+      <button className="chat-button">CHAT NOW!</button>
     </section>
   );
 }
